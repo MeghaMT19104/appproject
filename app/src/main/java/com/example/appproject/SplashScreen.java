dfe3857ea9b3610d9e99d685f6271c126e3d9ec5
chhavi <chhavi19117@iiitd.ac.in>
2019-10-09 21:50:41 +0530

    Added login status check and changed icon of app

--- app/src/main/java/com/example/appproject/SplashScreen.java
+++ app/src/main/java/com/example/appproject/SplashScreen.java
@@ -2,7 +2,9 @@ package com.example.appproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -16,13 +18,28 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
                 // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashScreen.this, LoginActivity.class);
+                // Start login activity if login status is false
+                Intent i;
+                if(!getLoginStatusFromPreferences())
+                {
+                    i = new Intent(SplashScreen.this, LoginActivity.class);
+                }
+                else
+                {
+                    i = new Intent(SplashScreen.this, About.class);
+                }
                 startActivity(i);
 
                 // close this activity
                 finish();
             }
         }, 2000);
+    }
+
+    private boolean getLoginStatusFromPreferences()
+    {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("pref_login", Context.MODE_PRIVATE); // 0 - for private mode
+        //SharedPreferences.Editor editor = pref.edit();
+        return pref.contains("emailid");
     }
 }
